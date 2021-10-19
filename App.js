907368acb743f8564216edef458cd1bd578e7cec
Fxnwar <92764636+Fxnwar@users.noreply.github.com>
2021-10-18 20:29:30 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -1,12 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './Components/Task';
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's Task</Text>
+      
+        <View style={styles.items}>
+          {/* This is where the task will go! */}
+          <img src={require('./assets/Images/PX426R.jpg')} className='logo' alt="" width="300" height="200"/>
+          <Task text={'Buy Strawberry'}/>
+          <img src={require('./assets/Images/banana.jpg')} className='logo' alt="" width="300" height="200"/>
+          <Task text={'Buy Banana'}/>
+          <img src={require('./assets/Images/apples.jpg')} className='logo' alt="" width="300" height="200"/>
+          <Task text={'Buy Apples'}/>
+          <img src={require('./assets/Images/grapes.jpg')} className='logo' alt="" width="300" height="200"/>
+          <Task text={'Buy Grapes'}/>
+          <img src={require('./assets/Images/Mango.jpg')} className='logo' alt="" width="300" height="200"/>
+          <Task text={'Buy Mango'}/>
+          <img src={require('./assets/Images/peaches.jpg')} className='logo' alt="" width="300" height="200"/>
+          <Task text={'Buy Peaches'}/>
+
+
+        </View>
+      
+      </View>
+      
     </View>
   );
 }
@@ -14,8 +39,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
+    
+
   },
+
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  items: {
+    marginTop: 30,
+  },
+
 });
